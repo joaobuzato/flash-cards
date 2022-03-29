208186ecf21f463029835dbf682ade0aaba6ec7d
Buzato <joaobuzato@gmail.com>
2022-03-29 09:11:01 -0300

    Adding the datafram french -> english

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from tkinter import *
+import pandas as pd
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_LANGUAGE = ('Ariel', 40, 'italic')
 FONT_WORD = ('Ariel', 60, 'bold')
+# ------------- Data ----------------
+
+df = pd.read_csv(filepath_or_buffer="data/french_words.csv")
+to_learn = df.to_dict(orient="records")
+word = random.choice(to_learn)
+
+# ---------- Buttons ---------------
+def next_card():
+    next_word = random.choice(to_learn)
+    card.itemconfig()
+
+
+
+
 
 window = Tk()
 window.title("Flash Card Game")
@@ -11,12 +27,12 @@ card.config(bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
 card.create_image(400, 261, image=card_front_img)
 card.create_text(400, 150, font=FONT_LANGUAGE, text="French")  # CHANGE LANGUAGE
-card.create_text(400, 263, font=FONT_WORD, text="Pardon")  # CHANGE WORD
+card.create_text(400, 263, font=FONT_WORD,text=word.get("French"))  # CHANGE WORD
 card.grid(row=0, column=0, columnspan=2)
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=next_card)
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 wrong_button.grid(row=1, column=0)
 
