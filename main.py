58aea5104e8a35466d21907dd0161caa19794bd6
Buzato <joaobuzato@gmail.com>
2022-03-30 07:56:33 -0300

    flip card

--- main.py
+++ main.py
@@ -11,10 +11,25 @@ to_learn = df.to_dict(orient="records")
 word = random.choice(to_learn)
 
 # ---------- Buttons ---------------
+
+
+def flip_card(next_word):
+
+    card.itemconfig(card_image, image=card_back_img)
+    card.itemconfig(card_title, text="English", fill='white')
+    card.itemconfig(card_word, text=next_word.get('English'), fill='white')
+
+
 def next_card():
+    global flip_timer
+    window.after_cancel(flip_timer)
     next_word = random.choice(to_learn)
-    card.itemconfig(card_title, text="French")
-    card.itemconfig(card_word, text=next_word.get('French'))
+    card.itemconfig(card_image, image=card_front_img)
+    card.itemconfig(card_title, text="French", fill='black')
+    card.itemconfig(card_word, text=next_word.get('French'), fill='black')
+    flip_timer = window.after(3000, flip_card, next_word)
+
+
 
 
 
@@ -26,9 +41,10 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 card = Canvas(width=800, height=526)
 card.config(bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
-card.create_image(400, 261, image=card_front_img)
-card_title = card.create_text(400, 150, font=FONT_LANGUAGE, text="French")  # CHANGE LANGUAGE
-card_word = card.create_text(400, 263, font=FONT_WORD,text=word.get("French"))  # CHANGE WORD
+card_back_img = PhotoImage(file="images/card_back.png")
+card_image = card.create_image(400, 261, image=card_front_img)
+card_title = card.create_text(400, 150, font=FONT_LANGUAGE, text="French") # CHANGE LANGUAGE
+card_word = card.create_text(400, 263, font=FONT_WORD, text=word.get("French"))  # CHANGE WORD
 card.grid(row=0, column=0, columnspan=2)
 right_img = PhotoImage(file="images/right.png")
 right_button = Button(image=right_img, highlightthickness=0, command=next_card)
@@ -37,8 +53,6 @@ wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 wrong_button.grid(row=1, column=0)
 
+flip_timer = window.after(1, next_card)
 
-
-
-next_card()
 window.mainloop()
